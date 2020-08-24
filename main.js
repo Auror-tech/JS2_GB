2abb78b77d0314d86e526a5e0a8a4a5f2eb548e3
Vladimir <exeWinter@gmail.com>
2020-08-24 20:50:27 +0300

    add classes, add method to calculate and display summary price of goods in bracket

--- main.js
+++ main.js
@@ -6,38 +6,73 @@ const goods = [
     { title: 'MANGO PEOPLE T-SHIRT', price: 450, imgSrc: 'img/bracket_3.jpg', color: 'YELLOW', size: 'IX', quantity: 4, shippingType: 'FREE'},
     { },
 ];
-const renderGoodsItem = (title = "SOON", price = 0, imgSrc = "img/soon_placeholder.jpg", color = "???", size = "?", quantity = "0", shippingType = "???") => {
-    return `<tr class="tr_br">
-                <td class="td_br td_br1">
-                    <img src="${imgSrc}" alt="" class="br_img">
-                    <div class="br_description">
-                        <div class="br_name">${title}</div>
-                        <div class="br_color">
-                        <span class="bold11_black">Color: </span>
-                        <span class="thin11_gray">${color}</span> <br>
-                        
-                        </div>
-                        <div class="br_size">
-                            <span class="bold11_black">Size:</span>
-                        <span class="thin11_gray">${size}</span>
-                        </div>
-                    </div>
-                </td>
-                <td class="td_br">&#36;${price}</td>
-                <td class="td_br">
-                    <input type="number" class="br_quantity" value="${quantity}">
-                </td>
-                <td class="td_br">${shippingType}</td>
-                <td class="td_br">&#36;${price*quantity}</td>
-                <td class="td_br">
-                    <img src="img/delete_icon.png" alt="" class="br_delete">
-                </td>
-            </tr>`;
-};
-  
-const renderGoodsList = (list) => {
-    const goodsList = list.map(item => renderGoodsItem(item.title, item.price, item.imgSrc, item.color, item.size, item.quantity, item.shippingType));
-    document.querySelector('.bracket_table').innerHTML = goodsList.join('');
+
+class GoodsItem {
+    constructor(title = "SOON", price = 0, imgSrc = "img/soon_placeholder.jpg", color = "???", size = "?", quantity = 0, shippingType = "???"){
+        this.title = title;
+        this.price = price;
+        this.imgSrc = imgSrc;
+        this.color = color;
+        this.size = size;
+        this.quantity = quantity;
+        this.shippingType = shippingType;
+    }
+    render() {
+        return `<tr class="tr_br">
+        <td class="td_br td_br1">
+            <img src="${this.imgSrc}" alt="" class="br_img">
+            <div class="br_description">
+                <div class="br_name">${this.title}</div>
+                <div class="br_color">
+                <span class="bold11_black">Color: </span>
+                <span class="thin11_gray">${this.color}</span> <br>
+                
+                </div>
+                <div class="br_size">
+                    <span class="bold11_black">Size:</span>
+                <span class="thin11_gray">${this.size}</span>
+                </div>
+            </div>
+        </td>
+        <td class="td_br">&#36;${this.price}</td>
+        <td class="td_br">
+            <input type="number" class="br_quantity" value="${this.quantity}">
+        </td>
+        <td class="td_br">${this.shippingType}</td>
+        <td class="td_br">&#36;${this.price*this.quantity}</td>
+        <td class="td_br">
+            <img src="img/delete_icon.png" alt="" class="br_delete">
+        </td>
+    </tr>`;
+    }
 }
-  
-renderGoodsList(goods);
+
+class GoodsList {
+    constructor () {
+        this.goods = [];
+    }
+    fetchGoods (){
+        this.goods = goods;
+    }
+    render () {
+        let html = "";
+        this.goods.forEach(({title, price, imgSrc, color, size, quantity, shippingType}) => {
+            const goodItem = new GoodsItem(title, price, imgSrc, color, size, quantity, shippingType);
+            html += goodItem.render();
+        });
+        document.querySelector ('.bracket_table').innerHTML = html;
+    }
+    calculateAndRenderSummary () {
+        let summ = 0;
+        this.goods.forEach(({price = 0, quantity = 1}) => {
+            summ += price*quantity;
+            console.log(price);
+        });
+        document.querySelector ('.sub_total').innerHTML = "Sub total&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&#36;" + summ;
+        document.querySelector ('.grand_total').innerHTML = "&#36;" + summ;
+    }
+}
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
+list.calculateAndRenderSummary ();
